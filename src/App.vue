9e9ffe0f13e450c75d970722e67b3937e8b04b53
Štefan Černý <stefan.cerny@gmail.com>
2022-06-03 13:59:52 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <h1>JSON Forms Vue 3</h1>
+  <div class="myform">
+    <json-forms
+      :data="data"
+      :renderers="renderers"
+      :schema="schema"
+      :uischema="uischema"
+      @change="onChange"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { JsonForms, JsonFormsChangeEvent } from "@jsonforms/vue";
+import {
+  defaultStyles,
+  mergeStyles,
+  vanillaRenderers,
+} from "@jsonforms/vue-vanilla";
+
+// mergeStyles combines all classes from both styles definitions into one
+const myStyles = mergeStyles(defaultStyles, { control: { label: "mylabel" } });
+
+const renderers = [
+  ...vanillaRenderers,
+  // here you can add custom renderers
+];
+import schema from "./forms/schema";
+import uischema from "./forms/uischema";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    JsonForms,
+  },
+  data() {
+    return {
+      // freeze renderers for performance gains
+      renderers: Object.freeze(renderers),
+      data: {
+        name: "Send email to Adrian",
+        description: "Confirm if you have passed the subject\nHereby ...",
+        done: true,
+        recurrence: "Daily",
+        rating: 3,
+      },
+      schema,
+      uischema,
+    };
+  },
+  methods: {
+    onChange(event: JsonFormsChangeEvent) {
+      this.data = event.data;
+    },
+  },
+  provide() {
+    return {
+      styles: myStyles,
+    };
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  margin-left: 120px;
+  margin-right: 120px;
+}
+</style>
